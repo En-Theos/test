074d220618dd9beae714fc90241f55e34903d7f0
EnTheos <theosenigma@gmail.com>
2021-07-12 15:32:21 +0300

    Отправка данных на сервер

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ parentBtnTabs.addEventListener('click', (event) => {
 
 // Таймер
 
-const deadline = "2021-07-11";
+const deadline = "2021-07-13";
 const timer = document.querySelector('.timer');
 
 function setTimer(endTime, parentTimer) {
@@ -47,6 +47,10 @@ function setTimer(endTime, parentTimer) {
 
     if (restTime <= 0) {
         clearInterval(timerInterwal);
+        daysBox.textContent = "00";
+        hoursBox.textContent = "00";
+        minutesBox.textContent = "00";
+        secondsBox.textContent = "00";
     }
 
     function getZero(number) {
@@ -64,11 +68,11 @@ setTimer(deadline, timer);
 const modal = document.querySelector(".modal");
 const btnOpenModal = document.querySelectorAll("[data-modal]"),
       btnCloseModal = document.querySelector("[data-close]");
-const timerOpenModal = setTimeout(codeShow, 5000);
+//const timerOpenModal = setTimeout(codeShow, 5000);
 
 
 function codeShow() {
-    clearTimeout(timerOpenModal);
+    //clearTimeout(timerOpenModal);
     modal.style.display = 'block';
     document.body.style.overflow = "hidden";
 }
@@ -88,7 +92,9 @@ modal.addEventListener('click', (event) => {
 });
 
 document.addEventListener('keydown', (event) => {
-   console.log(event);
+    if (event.code == "Escape") {
+        codeHiden();
+    }
 });
 
 window.addEventListener('scroll', function scrollOpenModal ()  {
@@ -99,4 +105,107 @@ window.addEventListener('scroll', function scrollOpenModal ()  {
         codeShow();
         window.removeEventListener('scroll', scrollOpenModal);
     }
+});
+
+// Динамическая генерация карточок на сайте
+
+const containerCardMenu = document.querySelector(".menu__field .container");
+
+class CardMenu {
+    constructor(src, alt, title, descr, price) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.tranfer = 27;
+        this.UAHConvert();
+    }
+
+    UAHConvert () {
+        this.price *= this.tranfer;
+    }
+     
+    generatorCardMenu () {
+        const div = document.createElement("div");
+        div.classList.add("menu__item");
+        
+        div.innerHTML = `
+            <img src="${this.src}" alt="${this.alt}">
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        `;
+
+        containerCardMenu.append(div);
+    }
+}
+
+new CardMenu(
+    "img/tabs/vegy.jpg", 
+    "vegy", 
+    'Меню "Фитнес"', 
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    229
+).generatorCardMenu();
+
+new CardMenu(
+    "img/tabs/elite.jpg", 
+    "elite", 
+    'Меню “Премиум”', 
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    550
+).generatorCardMenu();  
+                            
+new CardMenu(
+    "img/tabs/post.jpg", 
+    "post", 
+    'Меню "Постное"', 
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+    430
+).generatorCardMenu();
+
+// Отправка данных форм
+
+const forms = document.querySelectorAll("form");
+
+const message = {
+    success: 'Успешно отправлено',
+    load: 'Загрузка',
+    failed: 'Ошибка отправки'
+};
+
+forms.forEach(form => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const mesageBox = document.createElement("div");
+        mesageBox.classList.add("messageText");
+        mesageBox.textContent = message.load;
+        form.append(mesageBox);
+
+        const formData = new FormData(form);
+        const postJSON = {};
+        formData.forEach((value, key) => postJSON[key] = value);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader("Content-type", "application/json");
+        request.send(JSON.stringify(postJSON));
+
+        request.addEventListener('load', () => {
+            if (request.status == 200) {
+                mesageBox.textContent = message.success;
+                setTimeout(() => mesageBox.remove(), 2000);
+                form.reset();
+            } else {
+                mesageBox.textContent = message.failed;
+                setTimeout(() => mesageBox.remove(), 2000);
+            }
+        });
+    });
 });
